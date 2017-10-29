34cd275e1ccec1d98be08cce86a185d62665c1df
Imam Susanto <abc.imams@gmail.com>
2017-10-29 14:34:02 +0700

    Optimization + Dockerfile

---
+++ README.md
@@ -0,0 +1,6 @@
+# Ghost × Bootstrap 4
+
+Ghost Theme with default Bootstrap 4 Skin
+
+**Demo**
+
---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+  window.lozad().observe();
+});
